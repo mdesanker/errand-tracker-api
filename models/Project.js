9a51f00456109715e904156418557b3c4a3dd20b
Michael Desanker <mdesanker@gmail.com>
2022-01-17 07:02:06 -0500

    Create project model

--- models/Project.js
+++ models/Project.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProjectSchema = new Schema({});
+const ProjectSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
 
 module.exports = mongoose.model("Project", ProjectSchema);
