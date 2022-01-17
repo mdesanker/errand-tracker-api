0a396f616693cae336448d421f2e6aa3ca0618af
Michael Desanker <mdesanker@gmail.com>
2022-01-17 06:23:55 -0500

    Set up cors for dev

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
+
+app.use(cors({ origin: [/\localhost/], credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
 
