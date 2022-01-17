4dd2600de419277f9532ee967311eb7bad874944
Michael Desanker <mdesanker@gmail.com>
2022-01-17 09:53:13 -0500

    Add login route

--- routes/api/user.js
+++ routes/api/user.js
@@ -60,10 +60,65 @@ user.post("/register", [
       // Hash password
       user.password = await bcrypt.hash(password, 10);
 
-      console.log(user);
-
       // Save user in db
       await user.save();
+
+      // Return jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, process.env.KEY, { expiresIn: "24h" }, (err, token) => {
+        if (err) throw new Error(err);
+        res.json({ token });
+      });
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server error");
+    }
+  },
+]);
+
+// @route   POST /api/user/login
+// @desc    Login existing user
+// @access  Public
+user.post("/login", [
+  // Validate and sanitize input
+  check("email", "Email is required").trim().isEmail(),
+  check("password", "Password must be at least 6 characters").isLength({
+    min: 6,
+  }),
+
+  // Process request
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Find account
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Check correct password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
 
       // Return jwt
       const payload = {
