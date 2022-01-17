71c35dca9decd01079f5c3f8d260ae409c3ca53a
Michael Desanker <mdesanker@gmail.com>
2022-01-17 07:00:14 -0500

    Create errand model

--- models/Errand.js
+++ models/Errand.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ErrandSchema = new Schema({});
+const ErrandSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: Date, default: Date.now },
+  dueDate: { type: Date },
+  priority: {
+    type: String,
+    enum: ["None", "Low", "Medium", "High"],
+    default: "None",
+  },
+  project: { type: Schema.Types.ObjectId, ref: "Project" },
+});
 
 module.exports = mongoose.model("Errand", ErrandSchema);
