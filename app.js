2a4d5f6ceaac98e6735d27a3fd66f24c90a67d6e
Michael Desanker <mdesanker@gmail.com>
2022-01-17 06:20:53 -0500

    Connect to db

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 8000;
 
