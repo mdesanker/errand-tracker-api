efcabd528c318527f038f6ea43e69c01be6d1874
Michael Desanker <mdesanker@gmail.com>
2022-01-18 05:22:35 -0500

    Add functions to generate users to seed.js

--- routes/__test__/seed.js
+++ routes/__test__/seed.js
@@ -2,4 +2,32 @@ const faker = require("faker");
 
 const User = require("../../models/User");
 
-console.log("Seed.js running...");
+// TODO: How to seed mongodb memory server
+
+const users = [];
+const projects = [];
+const errands = [];
+
+// Generate specific user
+const generateSpecificUser = () => {
+  const user = new User({
+    username: "Greg",
+    email: "greg@example.com",
+    password: "password",
+    avatar: "",
+    friends: [],
+  });
+  users.push(user);
+};
+
+// Generate faker user
+const generateUser = () => {
+  const user = new User({
+    username: faker.internet.userName(),
+    email: faker.internet.exampleEmail(),
+    password: faker.internet.password(8),
+    avatar: faker.internet.avatar(),
+    friends: [],
+  });
+  users.push(user);
+};
