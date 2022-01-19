2b76ae282d1904501992fa8487f293f7b6acf25f
Michael Desanker <mdesanker@gmail.com>
2022-01-18 19:54:21 -0500

    Add test for get errands for spcific user

--- routes/__test__/errand.test.js
+++ routes/__test__/errand.test.js
@@ -62,8 +62,8 @@ describe("POST /api/errand/create", () => {
 });
 
 // Errand reading
-describe("errand read end points", () => {
-  it("success GET /api/errand/all", async () => {
+describe("GET /api/errand/all", () => {
+  it("return all errands in db", async () => {
     const res = await request(app)
       .get("/api/errand/all")
       .set("x-auth-token", token);
@@ -71,5 +71,29 @@ describe("errand read end points", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body[0]).toHaveProperty("title");
     expect(res.body[0]).toHaveProperty("author");
+  });
+});
+
+describe("GET /api/errand/:userid", () => {
+  it("return errands for specific user", async () => {
+    const res = await request(app)
+      .get("/api/errand/:userid")
+      .set("x-auth-token", token)
+      .send({ id: "61e71a80f0f8833ac7d5201d" });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body[0]).toHaveProperty("title");
+    expect(res.body[0]).toHaveProperty("author");
+  });
+
+  it("return error for invalid userid", async () => {
+    const res = await request(app)
+      .get("/api/errand/:userid")
+      .set("x-auth-token", token)
+      .send({ id: "invalidUserId" });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0]).toEqual("Invalid user id");
   });
 });
