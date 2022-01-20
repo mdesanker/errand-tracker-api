9c6e1e6915ad5a58dd5e01a1d73a0b77612aba77
Michael Desanker <mdesanker@gmail.com>
2022-01-20 14:02:11 -0500

    Expand db seed file for more complete testing

--- routes/__test__/seed.js
+++ routes/__test__/seed.js
@@ -9,32 +9,34 @@ const users = [];
 const projects = [];
 const errands = [];
 
-// Generate specific users
+////////////////////////////////////////
+/* GENERATE USERS */
+////////////////////////////////////////
+
 const generateGreg = () => {
   const user = new User({
-    _id: "61e71828c9cb2005247017c7",
     username: "Greg",
     email: "greg@example.com",
     password: "$2a$10$ijwz5joIBRc/.GSoOoYqtu3hxWu7AJSjlAUJapiEMunrtDe2Kme8m",
     avatar: "",
     friends: [],
+    _id: "61e71828c9cb2005247017c7",
   });
   users.push(user);
 };
 
 const generateGretta = () => {
   const user = new User({
-    _id: "61e7ec186394874272d11e67",
     username: "Gretta",
     email: "gretta@example.net",
     password: "$2a$10$NewMjfxNR9NW7Dbk6gLpjuIHsbxv6pAfL/sE6LXj.HkNP1zQ3Oo2W",
     avatar: "",
     friends: [],
+    _id: "61e7ec186394874272d11e67",
   });
   users.push(user);
 };
 
-// Generate faker user
 const generateUser = () => {
   const user = new User({
     username: faker.internet.userName(),
@@ -46,22 +48,51 @@ const generateUser = () => {
   users.push(user);
 };
 
-// Generate specific project
-const generateSpecificProject = () => {
+////////////////////////////////////////
+/* GENERATE PROJECTS */
+////////////////////////////////////////
+
+const generateGregProject = () => {
   const project = new Project({
-    title: "Shopping List",
+    title: "Solo project",
     author: users[0]._id, // Greg owns project
     description: faker.lorem.sentence(),
+    members: [], // No members
     _id: "61e7dd93ecec03286743e04e",
   });
   projects.push(project);
 };
 
-// Generate specific errand
-const generateSpecificErrand = () => {
-  const errand = new Errand({
-    title: faker.lorem.words(2),
+const generateGregAndGrettaProject = () => {
+  const project = new Project({
+    title: "Shared project",
+    author: users[0]._id, // Greg owns project
     description: faker.lorem.sentence(),
+    members: [users[1]._id], // Gretta is member
+    _id: "61e7dd93ecec03286743e04a",
+  });
+  projects.push(project);
+};
+
+////////////////////////////////////////
+/* GENERATE ERRANDS */
+////////////////////////////////////////
+
+const generateErrand = () => {
+  const errand = new Errand({
+    title: "Standalone errand",
+    description: "This errand is not associated with a project",
+    author: "61e71828c9cb2005247017c7",
+    priority: "Medium",
+    _id: "61e71a80f0f8833ac7d52011",
+  });
+  errands.push(errand);
+};
+
+const generateGregErrand = () => {
+  const errand = new Errand({
+    title: "Greg's errand",
+    description: "This is a greg only errand",
     author: "61e71828c9cb2005247017c7",
     priority: "High",
     project: "61e7dd93ecec03286743e04e",
@@ -70,8 +101,20 @@ const generateSpecificErrand = () => {
   errands.push(errand);
 };
 
+const generateGregAndGrettaErrand = () => {
+  const errand = new Errand({
+    title: "Greg and Gretta's errand",
+    description: "Gretta can help Greg with this errand",
+    author: "61e71828c9cb2005247017c7",
+    priority: "Low",
+    project: "61e7dd93ecec03286743e04a",
+    _id: "61e71a80f0f8833ac7d5201e",
+  });
+  errands.push(errand);
+};
+
 // Generate faker errand
-const generateErrand = (authorId) => {
+const generateIdErrand = (authorId) => {
   const priorities = ["None", "Low", "Medium", "High"];
 
   const random = Math.floor(Math.random() * priorities.length);
@@ -84,19 +127,36 @@ const generateErrand = (authorId) => {
   });
   errands.push(errand);
 };
+
+////////////////////////////////////////
+/* SEED DB FUNCTION */
+////////////////////////////////////////
+
 const seedDB = async () => {
-  // Generate specifics
+  // Generate users
   generateGreg();
   generateGretta();
-  generateSpecificProject();
-  generateSpecificErrand();
 
-  // Generate 3 randoms
   for (let i = 0; i < 3; i++) {
     generateUser();
   }
 
-  // Save users to db
+  // Generate projects
+  generateGregProject();
+  generateGregAndGrettaProject();
+
+  // Generate errands
+  generateErrand();
+  generateGregErrand();
+  generateGregAndGrettaErrand();
+
+  for (user of users) {
+    for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
+      generateErrand(user._id);
+    }
+  }
+
+  // Save items to db
   for (user of users) {
     try {
       await user.save();
@@ -105,14 +165,6 @@ const seedDB = async () => {
     }
   }
 
-  // Generate random number [1, 3] of errands per user
-  for (user of users) {
-    for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
-      generateErrand(user._id);
-    }
-  }
-
-  // Save projects to db
   for (project of projects) {
     try {
       await project.save();
@@ -121,7 +173,6 @@ const seedDB = async () => {
     }
   }
 
-  // Save errands to db
   for (errand of errands) {
     try {
       await errand.save();
