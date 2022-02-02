6177c46693a8841733a4ac1a44b2814ef8d2314e
Michael Desanker <mdesanker@gmail.com>
2022-02-01 21:13:10 -0500

    Update app for heroku

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/mongoConfig");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 
 const userRouter = require("./routes/api/user");
 const errandRouter = require("./routes/api/errand");
@@ -11,7 +13,14 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: [/\localhost/], credentials: true }));
+app.use(
+  cors({
+    origin: [/\localhost/, "https://mdesanker.github.io"],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+const mongoDB = process.env.MONGODB_URI || process.env.DEV_DB_URI;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
+    await mongoose.connect(mongoDB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
